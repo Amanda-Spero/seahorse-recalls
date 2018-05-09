1f0cab20bad1e203fdd30704ca92826f7851685f
Jason Kososki <jason.kososki@gmail.com>
2018-05-08 19:13:02 -0500

    updates env vars

--- config/config.js
+++ config/config.js
@@ -31,10 +31,10 @@ const hashOpt = {
 
 // TODO:  modify this to use environment variables
 const sql = {
-  database: 'project2db',
-  host: 'localhost',
-  username: 'root',
-  password: 'password',
+  database: process.env.database || 'project2db',
+  host: process.env.host || 'localhost',
+  username: process.env.username || 'root',
+  password: process.env.password || 'password',
 };
 
 
