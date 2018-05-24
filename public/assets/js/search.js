b4a48018ce2939ea70cf421b51b2a7e8056634c6
Amanda Helms <amandaspero@Amandas-MacBook-Pro.local>
2018-05-24 11:14:33 -0500

    fixing search page

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,115 +1,127 @@
+//import { ENETRESET } from "constants";
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+
 $(document).ready(function () {
 
 
-
-  //may need to take out all $(this).attr and .val().trim()
-  var makeInput = $("#make-input");
-  var modelInput = $("#model-input");
-  var yearInput = $("#year-input");
-
-
-  /**
-   * takes API data (JSON/object) and turns it into elements on the page
-   * @param {object} NHTSAData - object containing NYT API data
-   */
-
-  function updatePage(NHTSAData) {
-
-    console.log(NHTSAData);
-    console.log("------------------------------------");
-
-    for (var i = 0; i < numCars; i++) {
-      // Get specific article info for current index
-      var car = NHTSAData.response.docs[i]; //response.docs??  api shows "results"//
-      console.log(car);
-
-      var carCount = i + 1;
-
-      var $carList = $("<ul>");
-      $carList.addClass("list-group");
-
-      $("#table-row").append($carList);
-
-      var resultYear = car.ModelYear;
-      console.log(resultYear);
-      var $carListItem = $("<li class='list-group-item carresultYear'>");
-
-
-      if (resultYear && resultYear.main) { ///???.MAIN????///////
-        console.log(resultYear.main);
-        $carListItem.append(
-          "<span class='label label-primary'>" +
-          carCount +
-          "</span>" +
-          "<strong> " +
-          resultYear.main +
-          "</strong>"
-        );
-      }
-
-
-      var resultMake = car.Make;
-
-      if (resultMake && resultMake.original) { ///????.original.?????/////
-        console.log(resultMake.original);
-        $carListItem.append("<h5>" + resultMake.original + "</h5>");
-      }
-
-
-      // Log Model, and append to document if exists
-      var resultModel = car.Model;
-      console.log(car.Model);
-      if (resultModel) {
-        $carListItem.append("<h5>Section: " + resultModel + "</h5>");
-      }
-    }
-  }
-
-  // Function to empty out the articles
   function clear() {
-    $("#table-row").empty();
+    $("#component-name").empty();
+    $("#recall-panel").empty();
   }
 
-  // CLICK HANDLERS
-  // ==========================================================
-
-  // .on("click") function associated with the Search Button
   $("#search-button").on("click", function (event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
+
     event.preventDefault();
 
+    var makeInput = $("#make-input");
+    var modelInput = $("#model-input");
+    var yearInput = $("#year-input");
 
-
-    // Empty the region associated with the articles
-    clear();
-
-    // Build the query URL for the ajax request to the NYT API
-    //https://webapi.nhtsa.gov/api/Recalls/vehicle/
-    // var queryURL = "https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/" + yearInput + "/make/" + makeInput + "?format=json?";
-    // /api/search/year/2000/make/ford/model/mustang
     var queryURL = "/api/search/year/" + yearInput.val().trim() + "/make/" + makeInput.val().trim() + "/model/" + modelInput.val().trim();
 
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
     $.ajax({
         url: queryURL,
         method: "GET"
       })
       .then(resp => {
-        console.log(resp);
-        updatePage(resp);
-      });
+
+       // if {}
+       var results = resp.Results;
+
+        var recallComponent = (results[0].Component);
+        var recallSummary = (results[0].Summary);
+        var recallConequence = (results[0].Conequence);
+        var recallRemedy = (results[0].Remedy);
+        //var recallCount = j + 1;
+        //console.log(results.length);
+    
+        $("#recalls-box > #recall-component").append(recallComponent);
+        $("#recall-summary").append("SUMMARY:   " + recallSummary + "<hr><hr>");
+        $("#recall-conequence").append("CONSEQUENCE:   " + recallConequence + "<hr><hr>");
+        $("#recall-remedy").append("REMEDY:   " + recallRemedy);
+
+///
+
+        var recallComponent2 = (results[1].Component);
+        var recallSummary2 = (results[1].Summary + "<hr>");
+        var recallConequence2 = (results[1].Conequence + "<hr>");
+        var recallRemedy2 = (results[1].Remedy);
+ 
+    
+        $("#recalls-box > #recall-component2").append(recallComponent2);
+        $("#recall-summary2").append("SUMMARY:   " + recallSummary2 + "<hr>");
+        $("#recall-conequence2").append("CONSEQUENCE:   " + recallConequence2 + "<hr>");
+        $("#recall-remedy2").append("REMEDY:   " + recallRemedy2);
+
+///
+
+        var recallComponent3 = (results[2].Component);
+        var recallSummary3 = (results[2].Summary + "<hr>");
+        var recallConequence3 = (results[2].Conequence + "<hr>");
+        var recallRemedy3 = (results[2].Remedy);
+        //var recallCount = j + 1;
+        //console.log(results.length);
+
+        $("#recalls-box > #recall-component3").append(recallComponent3);
+        $("#recall-summary3").append("SUMMARY:   " + recallSummary3 + "<hr>");
+        $("#recall-conequence3").append("CONSEQUENCE:   " + recallConequence3 + "<hr>");
+        $("#recall-remedy3").append("REMEDY:   " + recallRemedy3);
+
+///
+
+        var recallComponent4 = (results[3].Component);
+        var recallSummary4 = (results[3].Summary + "<hr>");
+        var recallConequence4 = (results[3].Conequence + "<hr>");
+        var recallRemedy4 = (results[3].Remedy);
+        //var recallCount = j + 1;
+        //console.log(results.length);
+
+        $("#recalls-box > #recall-component4").append(recallComponent4);
+        $("#recall-summary4").append("SUMMARY:   " + recallSummary4 + "<hr>");
+        $("#recall-conequence4").append("CONSEQUENCE:   " + recallConequence4 + "<hr>");
+        $("#recall-remedy4").append("REMEDY:   " + recallRemedy4);
+
+///
+
+        var recallComponent5 = (results[4].Component);
+        var recallSummary5 = (results[4].Summary);
+        var recallConequence5 = (results[4].Conequence);
+        var recallRemedy5 = (results[4].Remedy);
+        //var recallCount = j + 1;
+        //console.log(results.length);
+
+        $("#recalls-box > #recall-component5").append(recallComponent5);
+        $("#recall-summary5").append("SUMMARY:   " + recallSummary5 + "<hr>");
+        $("#recall-conequence5").append("CONSEQUENCE:   " + recallConequence5 + "<hr>");
+        $("#recall-remedy5").append("REMEDY:   " + recallRemedy5);
 
 
-  });
-
-  //  .on("click") function associated with the clear button
-  $("#clear-all").on("click", clear);
-
-
-
-
-
+  })
 })
+})
+
+
+
+
+
+
+
+
+
+  
